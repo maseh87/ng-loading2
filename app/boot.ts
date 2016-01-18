b97163d9ccd4e5c14058f140f2a865c0e8c36c1b
Mason Hargrove <maseh87@gmail.com>
2016-01-18 11:04:37 -0500

    bootstrapped

--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './ngLoading.component'
+import {HTTP_PROVIDERS, Http} from 'angular2/http'
+
 
 bootstrap(AppComponent);
